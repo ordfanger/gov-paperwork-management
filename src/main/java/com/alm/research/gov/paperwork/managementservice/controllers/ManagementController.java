a580c97f29e104af3750fd5af6dc01bb1c63e1ae
Oleksandr Mykolaichuk <alexmix3000@gmail.com>
2020-04-25 20:08:12 +0200

    added introspect

--- src/main/java/com/alm/research/gov/paperwork/managementservice/controllers/ManagementController.java
+++ src/main/java/com/alm/research/gov/paperwork/managementservice/controllers/ManagementController.java
@@ -9,10 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.IOException;
@@ -40,5 +37,12 @@ public class ManagementController {
             @RequestBody TokenRequestModel creds
     ) throws IOException {
         return ResponseEntity.ok(tokenService.getAccessToken(creds));
+    }
+
+    @PostMapping(
+            value = "/introspect"
+    )
+    public void isTokenValid() {
+        // For better token validation use okta /introspect
     }
 }
